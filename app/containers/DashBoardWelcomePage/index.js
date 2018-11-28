13c403480fec2438f0c2c815cae87cc43c92f6b0
rishav.0727 <rishav.0727@gmail.com>
2018-11-28 12:36:26 +0530

    Fixing build

--- app/containers/DashBoardWelcomePage/index.js
+++ app/containers/DashBoardWelcomePage/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { Redirect, Link  } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Modal, Button } from 'react-bootstrap';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
